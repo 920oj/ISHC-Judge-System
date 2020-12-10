eb702e6c1a3e1c56fda85f138c972d2f2d9f38a9
Junya Oiwa <0920oiwa@gmail.com>
2020-12-11 02:43:36 +0900

    [add]scoreエンドポイント実装

--- router/score.js
+++ router/score.js
@@ -2,7 +2,40 @@ const models = require("../models")
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  // スコア計算
+  try {
+    const Teams = await models.teams.findAll({
+      include: [{
+        model: models.judges,
+        required: false
+      }],
+      order: [
+        ['id', 'ASC']
+      ],
+    });
+
+    let result = [];
+
+    Teams.forEach(team => {
+      let score = 0;
+      team.judges.forEach(judge => {
+        score += judge.score;
+      })
+      result.push({
+        team_id: team.id,
+        team_name: team.name,
+        lab_name: team.lab_name,
+        score: score,
+      })
+    })
+
+    res.json(result);
+  }
+  catch {
+    res.status(500);
+    res.json({
+      err: "Internal Server Error."
+    })
+  }
 })
 
 router.get("/:id", async (req, res, next) => {
