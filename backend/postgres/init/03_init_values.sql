59ec7900103e9daef0e4901919c6526c4cb0ec77
Junya Oiwa <0920oiwa@gmail.com>
2020-12-19 02:13:53 +0900

    [update]ipアドレスを固定化

--- backend/postgres/init/03_init_values.sql
+++ backend/postgres/init/03_init_values.sql
@@ -6,18 +6,18 @@ INSERT INTO teams(name, lab_name) VALUES
   ('TEAM PIYOPIYO', '宮地研究室');
 
 INSERT INTO judges(team_id, question_id, answer, correct_flg, score, language, memory, answer_time, msg, created_at, updated_at) VALUES
-  ('2', '1', 'console.log(''hoge'')', 1, 300, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp),
-  ('3', '1', 'console.log(''hoge'')', 1, 300, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp),
-  ('2', '2', 'console.log(''hoge'')', 1, 500, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp),
-  ('1', '1', 'console.log(''hoge'')', 0, 300, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp),
-  ('2', '3', 'console.log(''hoge'')', 1, 300, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp),
-  ('2', '1', 'console.log(''hoge'')', 1, 700, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp),
-  ('3', '1', 'console.log(''hoge'')', 1, 300, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp),
-  ('2', '1', 'console.log(''hoge'')', 1, 300, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp),
-  ('2', '5', 'console.log(''hoge'')', 0, 200, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp),
-  ('2', '2', 'console.log(''hoge'')', 1, 300, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp),
-  ('1', '1', 'console.log(''hoge'')', 1, 300, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp),
-  ('2', '4', 'console.log(''hoge'')', 0, 500, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp),
-  ('1', '1', 'console.log(''hoge'')', 1, 300, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp),
-  ('2', '1', 'console.log("hoge")', 1, 300, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp),
-  ('2', '1', 'console.log(''hoge'')', 1, 300, 'JavaScript', '3200KB', 1000, '', current_timestamp, current_timestamp);
+  ('2', '1', 'console.log(''hoge'')', 1, 300, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp),
+  ('3', '1', 'console.log(''hoge'')', 1, 300, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp),
+  ('2', '2', 'console.log(''hoge'')', 1, 500, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp),
+  ('1', '1', 'console.log(''hoge'')', 0, 300, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp),
+  ('2', '3', 'console.log(''hoge'')', 1, 300, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp),
+  ('2', '1', 'console.log(''hoge'')', 1, 700, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp),
+  ('3', '1', 'console.log(''hoge'')', 1, 300, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp),
+  ('2', '1', 'console.log(''hoge'')', 1, 300, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp),
+  ('2', '5', 'console.log(''hoge'')', 0, 200, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp),
+  ('2', '2', 'console.log(''hoge'')', 1, 300, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp),
+  ('1', '1', 'console.log(''hoge'')', 1, 300, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp),
+  ('2', '4', 'console.log(''hoge'')', 0, 500, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp),
+  ('1', '1', 'console.log(''hoge'')', 1, 300, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp),
+  ('2', '1', 'console.log("hoge")', 1, 300, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp),
+  ('2', '1', 'console.log(''hoge'')', 1, 300, 'JavaScript', 3.2, 1000, '', current_timestamp, current_timestamp);
